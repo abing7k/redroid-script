ef477006e85db2131ec7492e9ea2baf67b8a5054
abing7k <91085461+abing7k@users.noreply.github.com>
2024-01-17 02:51:50 +0800

    Update gapps.py

--- stuffs/gapps.py
+++ stuffs/gapps.py
@@ -5,9 +5,9 @@ from tools.helper import get_download_dir, host, print_color, run, bcolors
 
 class Gapps(General):
     dl_links = {
-            "x86_64": ["https://cfhcable.dl.sourceforge.net/project/opengapps/x86_64/20220503/open_gapps-x86_64-10.0-pico-20220503.zip", "5fb186bfb7bed8925290f79247bec4cf"],
+            "x86_64": ["https://nchc.dl.sourceforge.net/project/opengapps/x86_64/20220503/open_gapps-x86_64-10.0-pico-20220503.zip", "5fb186bfb7bed8925290f79247bec4cf"],
             "x86": ["https://cfhcable.dl.sourceforge.net/project/opengapps/x86/20220503/open_gapps-x86-10.0-pico-20220503.zip", "7fc75ec9bdca8def07bad306345ce877"],
-            "arm64-v8a": ["https://cfhcable.dl.sourceforge.net/project/opengapps/arm64/20220503/open_gapps-arm64-10.0-pico-20220503.zip", "2feaf25d03530892c6146687ffa08bc2"],
+            "arm64-v8a": ["https://nchc.dl.sourceforge.net/project/opengapps/arm64/20220503/open_gapps-arm64-10.0-pico-20220503.zip", "2feaf25d03530892c6146687ffa08bc2"],
             "armeabi-v7a": ["https://cfhcable.dl.sourceforge.net/project/opengapps/arm/20220215/open_gapps-arm-10.0-pico-20220215.zip", "1d00ffa4594734d477b10f2e0ee19c0b"]
         }
     arch = host()
@@ -25,11 +25,11 @@ class Gapps(General):
         "googlepixelconfig-common.tar.lz"
         ]
 
-    if arch == ('arm64-v8a', 64):
+    if arch == ('x86_64', 64):
         skip_1 = 'setupwizarddefault-x86_64.tar.lz'
         skip_2 = "setupwizardtablet-x86_64.tar.lz"
         
-    if arch == ('x86_64', 64):
+    if arch == ('arm64-v8a', 64):
         skip_1 = 'setupwizarddefault-arm64.tar.lz'
         skip_2 = "setupwizardtablet-arm64.tar.lz"
     skip = [
